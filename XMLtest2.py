fd946893384168c15aac97918d95b15ce02ab06b
chohwanhee(lggram) <great1625@gmail.com>
2024-05-22 13:24:58 +0900

    편의점 리스트, 편의점 정보, 시 편의점 통계, 지도 이미지 캔버스 생성

--- XMLtest2.py
+++ XMLtest2.py
@@ -44,28 +44,44 @@ class ConvenienceStore:
 
         frame = Frame(window)
         frame.pack()
+
+        frame1 = Frame(frame)
+        frame1.pack()
+
+        frame2 = Frame(frame1)
+        frame2.pack(side=LEFT)
+
+
         # 편의점 목록 리스트박스 생성
-        self.store_list = Listbox(frame, width=60)
-        self.store_list.pack()
+        self.store_list = Listbox(frame2, width=50)
+        self.store_list.pack(side=LEFT)
 
         # 스크롤바 생성
-        scrollbar = Scrollbar(frame)
+        scrollbar = Scrollbar(frame2)
         scrollbar.pack(side=RIGHT, fill=Y)
 
         # 스크롤바와 편의점 목록 연결
         self.store_list.config(yscrollcommand=scrollbar.set)
         scrollbar.config(command=self.store_list.yview)
 
-
         self.si_combo.bind("<<ComboboxSelected>>", self.on_si_select)
 
         self.show_stores()
 
-        window.mainloop()
+        # 편의점 정보 출력 캔버스 생성
+        con_info = Canvas(frame1, width=300, height=200, bg='white')
+        con_info.pack(side=RIGHT)
 
         # 통계 캔버스 생성
-        # canvas = Canvas(window, width=300, height=300)
-        # canvas.pack(side=LEFT)
+        con_type = Canvas(frame, width=350, height=300, bg='white')
+        con_type.pack(side=LEFT)
+
+        # 지도 이미지 캔버스 생성
+        map_img = Canvas(frame, width=300, height=300, bg='white')
+        map_img.pack(side=RIGHT)
+
+        window.mainloop()
+
 
     def show_stores(self):
         self.store_list.delete(0, END)
@@ -73,14 +89,17 @@ class ConvenienceStore:
         si_name = self.selected_si.get()
         stores_in_si = [store for store in self.stores if store['si'] == si_name]
 
-        # 병원 목록에 추가
+        # 편의점 목록에 추가
         for store in stores_in_si:
             self.store_list.insert(END, f"{store['name']}")
 
     def on_si_select(self,event):
         self.show_stores()
 
+    def show_info(self, conv):
+        pass
 
 
+ConvenienceStore()
 
 
